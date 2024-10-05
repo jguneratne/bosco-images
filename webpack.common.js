fba0f4afa9956d1d8ff938f32f028e65f47cf1fd
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-10-04 21:52:35 -0400

    Remove favicons from this repo

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,5 @@
 const path = require("path");
 const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
-const {
-  FaviconsBundlerPlugin,
-} = require("html-bundler-webpack-plugin/plugins");
 
 module.exports = {
   // mode: "development",
@@ -103,33 +100,6 @@ module.exports = {
         views: [
           "src/partials", // relative path
           path.join(__dirname, "src/partials"), // absolute path
-        ],
-      },
-    }),
-    new FaviconsBundlerPlugin({
-      enabled: "auto", // true, false, auto - generate favicons in production mode only
-      // favicons configuration options, see https://github.com/itgalaxy/favicons#usage
-
-      faviconOptions: {
-        path: "assets/imgs/icons", // favicons output path relative to webpack output.path
-        background: "#fff",
-        appleStatusBarStyle: "fff",
-        orientation: "any",
-        icons: {
-          android: true, // Create Android homescreen icon.
-          appleIcon: true, // Create Apple touch icons.
-          appleStartup: false, // Create Apple startup images.
-          favicons: true, // Create regular favicons.
-          windows: true, // Create Windows 8 tile icons.
-          yandex: true, // Create Yandex browser icon.
-        },
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(png|jpe?g|ico|svg)$/,
-            type: "asset/resource",
-          },
         ],
       },
     }),
